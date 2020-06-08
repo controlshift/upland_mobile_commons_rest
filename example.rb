69e8916b90e0d5e7a9a16875fa3452b6ade406c2
Nathan Woodhull <woodhull@gmail.com>
2020-06-08 14:57:56 -0400

    Fix example testing script for gem.

--- example.rb
+++ example.rb
@@ -1,10 +1,13 @@
-require_relative 'lib/upland_mobile_commons_rest'
+  
+$LOAD_PATH << File.join(File.dirname(__FILE__), 'lib')
+
+require 'upland_mobile_commons_rest'
 require 'dotenv'
 require 'byebug'
 
 Dotenv.load('.env')
 
-client = UplandMoblieCommons.new(username: ENV['USERNAME'], password: ENV['PASSWORD'] ) # rubocop:disable Lint/UselessAssignment
+client = UplandMobileCommonsRest.new(username: ENV['USERNAME'], password: ENV['PASSWORD'] ) # rubocop:disable Lint/UselessAssignment
 
 puts "Ready to call MobileCommons API using 'client' object"
 byebug # rubocop:disable Lint/Debugger
